8ff5f5a245b8d916146663c37177d9e9e6308988
Jessweb87 <jprosper87@gmail.com>
2021-12-03 11:20:01 -0500

    created useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,20 +1,9 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react"
 
-export default function useForm(key, initialValue) {
-    const [Value, setValue] = useState(() => {
+const useForm = () => {
+  const [values, setValues] = useState()
 
-        const storedItem = key;
-
-        return storedItem;
-    });
-
-    const setTheValue = value => {
-        // doesn't really do much without the local storage
-        setValue(value);
-        console.log(setValue, "setValue");
-
-    };
-
-    return [Value, setTheValue];
+  return [values, setValues]
 }
+export default useForm;
